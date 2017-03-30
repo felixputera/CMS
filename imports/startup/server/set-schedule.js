2c252acc860fbe005f01c01e47606547b96e7f92
felixputera <felixputera@gmail.com>
2017-03-30 11:36:47 +0800

    backend milestone

--- imports/startup/server/set-schedule.js
+++ imports/startup/server/set-schedule.js
@@ -1,17 +1,43 @@
 import { Meteor } from 'meteor/meteor'
+import { SyncedCron } from 'meteor/percolate:synced-cron'
+
+import { sendEmail, postFacebook, postTwitter } from '../../utils/social-media.js'
+
 
 SyncedCron.add({
-  name: 'Update regularly nea psi data',
-  schedule: function(parser) {
-    // parser is a later.parse object
-    return parser.recur().on(15).minute();
-  },
-  job: function() {
-    Meteor.call('psi.fetch');
-  }
+    name: 'Update regularly nea psi data',
+    schedule: (parser) => {
+        // parser is a later.parse object
+        return parser.recur().on(15).minute();
+    },
+    job: () => {
+        Meteor.call('psi.fetch');
+    }
+});
+
+SyncedCron.add({
+    name: 'Send regular PM report',
+    schedule: (parser) => {
+        return parser.text('every 30 minutes');
+    },
+    job: () => {
+        sendEmail("pentium.pm@mail.com", Meteor.call('pmReport'));
+    }
+});
+
+SyncedCron.add({
+    name: 'Send regular social media update',
+    schedule: (parser) => {
+        return parser.text('every 1 hour');
+    },
+    job: () => {
+        const socMedMessage = Meteor.call('pmReport');
+        postTwitter(socMedMessage);
+        postFacebook(socMedMessage);
+    }
 });
 
 Meteor.startup(() => {
-  Meteor.call('psi.fetch');
-  SyncedCron.start();
+    Meteor.call('psi.fetch');
+    SyncedCron.start();
 });
--- imports/utils/social-media.js
+++ imports/utils/social-media.js
@@ -5,38 +5,39 @@ import Twit from 'twit';
 const keys = JSON.parse(Assets.getText("api-key.json"));
 
 const T = new Twit({
-	consumer_key: keys.twitter.consumer_key,
-	consumer_secret: keys.twitter.consumer_secret,
-	access_token: keys.twitter.access_token,
-	access_token_secret: keys.twitter.access_token_secret,
-	timeout_ms: 60*1000,  // optional HTTP request timeout to apply to all requests.
+    consumer_key: keys.twitter.consumer_key,
+    consumer_secret: keys.twitter.consumer_secret,
+    access_token: keys.twitter.access_token,
+    access_token_secret: keys.twitter.access_token_secret,
+    timeout_ms: 60 * 1000,  // optional HTTP request timeout to apply to all requests.
 });
 
 export const postFacebook = (text) => {
-	let pageAccessToken = keys.facebook;
-	return HTTP.post("https://graph.facebook.com/v2.8/286242935138459/feed?message=" + text + "&access_token=" + pageAccessToken);
+    let pageAccessToken = keys.facebook;
+    return HTTP.post("https://graph.facebook.com/v2.8/286242935138459/feed?message=" + text + "&access_token=" + pageAccessToken);
+    return console.log("Twitter tweeted");
 };
 
-export const sendEmail = (text) => {
-	let from = "pentium.cms@gmail.com";
-	let to = "pentium.pm@mail.com";
-	let subject = "Half-hourly Update from CMS";
+export const sendEmail = (recipient, text) => {
+    let from = "pentium.cms@gmail.com";
+    let to = recipient /*"pentium.pm@mail.com"*/;
+    let subject = "Half-hourly Update from CMS";
 
-	this.unblock();
+    this.unblock();
 
-	Email.send({
-		to: to,
-		from: from,
-		subject: subject,
-		text: text
-	});
+    Email.send({
+        to: to,
+        from: from,
+        subject: subject,
+        text: text
+    });
 
 	return console.log("Email sent");
 };
 
 export const postTwitter = (text) => {
     T.post('statuses/update', { status: text }, function(err, data, response) {
-	    console.log(data);
-	});
-	return console.log("Twitter sent");
+        console.log(data);
+    });
+    return console.log("Twitter tweeted");
 };
